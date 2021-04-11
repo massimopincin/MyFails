b18d69494eb05ae12ecca81621c0924a05242fd5
Replit user <replituser@example.com>
2021-04-11 14:41:43 +0000

    almost finished figures

--- chess.py
+++ chess.py
@@ -1,5 +1,28 @@
-#board
+def rmoves():
+    res=[]
+    for i in range(8):
+        res.append([i,0,0,0])
+        res.append([-i,0,0,0])
+        res.append([0,i,0,0])
+        res.append([0,-i,0,0])
+    return res
 
+def qmoves():
+    res=[]
+    for i in range(8):
+        res.append([i,0,0,0])
+        res.append([-i,0,0,0])
+        res.append([0,i,0,0])
+        res.append([0,-i,0,0])
+        res.append([0,0,i,0])
+        res.append([0,0,-i,0])
+        res.append([0,0,0,i])
+        res.append([0,0,0,-i])
+    return res
+
+
+def nmoves():
+    return [[2,1,0,0],[1,2,0,0],[-1,2,0,0],[1,-2,0,0],[-1,-2,0,0],[-2,1,0,0],[2,-1,0,0],[-2,-1,0,0]]
 
 def print_board(h):
     for i in range(8):
@@ -20,5 +43,21 @@ h=["R","N","B","K","Q","B","N","R"]
 
 list1=[a,b,c,d,e,f,g,h]
 
-print_board(list1)
+class Figure:
+    def __init__(self,name,move,capture):
+        self.name=name
+        self.move=move
+        self.capture=capture
+    
+rook=Figure("R",rmoves(),rmoves())
+knight=Figure("N",nmoves(),nmoves())
+queen=Figure("Q",qmoves(),qmoves())
+pawn=Figure("P",[[0,1,0,0]],[[0,0,1,0],[0,0,0,1]])
+king=Figure("K",[])
+
+
+print(rook.move)
+
+
+
     
